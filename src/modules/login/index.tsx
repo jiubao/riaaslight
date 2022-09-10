29761dccfba54ea0d6b74de485f40de58c13ecdd
jiubao <xia.ldj@gmail.com>
2022-09-10 18:15:46 +0800

    login page

--- src/modules/login/index.tsx
+++ src/modules/login/index.tsx
@@ -1,4 +1,3 @@
-import { Typography } from '@mui/material'
 import React, { useState } from 'react'
 import { useLinkedIn } from 'react-linkedin-login-oauth2'
 import linkedin from 'react-linkedin-login-oauth2/assets/linkedin.png'
@@ -6,66 +5,57 @@ import { LINKEDIN_CLIENTID } from '../../constants'
 import { authService } from '../../services/auth'
 import './index.scss'
 import store from 'store'
+import { useNavigate } from 'react-router-dom'
+import { LR } from '../../components/layout/lr'
+import { LogoIcon } from '../../components/icons'
 
 const PREFIX = 'Login'
 
+const Left = () => {
+  return (
+    <div className={`${PREFIX}-left fulfilled`}>
+      <img
+        src="https://s.cn.bing.net/th?id=OHR.MidAutumn2022_ZH-CN9825550508_1920x1080.jpg&rf=LaDigue_1920x1080.jpg"
+        alt=""
+      />
+      <div className={`${PREFIX}-mask fulfilled`}></div>
+      <span className="center">ROG PICTURES UPDATE DAILY</span>
+    </div>
+  )
+}
+
 export const Login: React.FC = () => {
-  const [code, setCode] = React.useState('')
-  const [errorMessage, setErrorMessage] = React.useState('')
-  const [token, setToken] = useState('')
+  const navigate = useNavigate()
 
   const { linkedInLogin } = useLinkedIn({
     clientId: LINKEDIN_CLIENTID,
     redirectUri: `${window.location.origin}/linkedin`,
     onSuccess: async (code) => {
       console.log(code)
-      setCode(code)
-      setErrorMessage('')
       const linkedInRes = await authService.linkedinCallback(code)
       const authToken = await authService.convertToken(linkedInRes.access_token)
-      setToken(authToken.access_token)
       store.set('access_token', authToken.access_token)
+      navigate('/')
     },
     scope: 'r_emailaddress r_liteprofile',
     onError: (error) => {
       console.log(error)
-      setCode('')
-      setErrorMessage(error.errorMessage)
     },
   })
 
   return (
-    <div className={PREFIX}>
-      <Typography component="h1">
-        <b>Welcome!</b>
-      </Typography>
-      <Typography>Sign in to continue</Typography>
-      <img
-        onClick={linkedInLogin}
-        src={linkedin}
-        alt="Log in with Linked In"
-        style={{ maxWidth: '180px', cursor: 'pointer' }}
-      />
-
-      {!code && <div>No code</div>}
-      {code && (
-        <div>
-          <div>Authorization Code: {code}</div>
-          <div>
-            Follow
-            <a
-              target="_blank"
-              href="https://docs.microsoft.com/en-us/linkedin/shared/authentication/authorization-code-flow?context=linkedin%2Fconsumer%2Fcontext&tabs=HTTPS#step-3-exchange-authorization-code-for-an-access-token"
-              rel="noreferrer"
-            >
-              this
-            </a>
-            to continue
-          </div>
+    <LR className={PREFIX} left={<Left />} percent={68}>
+      <div className={`${PREFIX}-right fulfilled flexCore`}>
+        <div className={`${PREFIX}-logo`}>
+          <LogoIcon width="98" height="29" viewBox="0 0 98 29" />
         </div>
-      )}
-      {token && <div>token: {token}</div>}
-      {errorMessage && <div>{errorMessage}</div>}
-    </div>
+        <img
+          onClick={linkedInLogin}
+          src={linkedin}
+          alt="Log in with Linked In"
+          style={{ maxWidth: '180px', cursor: 'pointer' }}
+        />
+      </div>
+    </LR>
   )
 }
